383a2a3ba8164f9d2fd55434f6807187ae8f435f
Llalo33 <147984046+Llalo33@users.noreply.github.com>
2023-10-30 16:09:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import s from './arrays.js'
 // import APP_VERSION, DEVELOPER from './constants'
 
 // можно давать любое имя при импорте, если экспорт был по умолчанию
-import {theBestFunction} from './math.js'
+import theBestFunction from './function.js'
+import {add , div} from './math.js'
 
 console.log(theBestFunction());
 
-
+console.log(add(), div())
 console.log(s()
