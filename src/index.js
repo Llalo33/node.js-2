d26b6b9c9b7b5c68fa5aeedd8a2fa80b56949d6b
Llalo33 <147984046+Llalo33@users.noreply.github.com>
2023-10-30 15:56:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 // если экспорт был именованный, то при импорте указываются точные названия и используются синтаксис с {}
 import s from './arrays.js'
-
 // ❌ такая запись не верна
 // import APP_VERSION, DEVELOPER from './constants'
 
 // можно давать любое имя при импорте, если экспорт был по умолчанию
-import theBestFunction from './function.js'
+import theBestFunction from './math.js'
 
 console.log(theBestFunction());
 
 
-console.log(s)
+console.log(s()
